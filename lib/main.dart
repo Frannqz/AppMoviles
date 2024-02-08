e9b3c79e586704ba8e93c69909fb17d4556427e2
Francisco Gonzalez <paco_glez02@hotmail.com>
2024-02-07 22:29:05 -0600

    Commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Practica 1',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+        ),
+        drawer: Drawer(),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red,
+          onPressed: () {},
+          child: Icon(Icons.ads_click),
+        ),
+        body: Center(
+          child: Image.network(
+            'https://lh3.googleusercontent.com/proxy/SMkENNRzq9pMmPk7tTgKdQlT5dHg7hyEkXziPvxqACf8hr_WWZO83_gKah9UbUXiFSZFjv7dZQoET9vIC4JvAJOlE-ULMk4ku4LaJCWK4o4-MT-GJLVJFpQHpfTC4I0AvrWt2FvC',
+            height: 150,
+          ),
+        ),
+      ),
+    );
+  }
+}
