819785c5e0063e777cb1feecf9990bea34535e9c
Francisco Gonzalez <paco_glez02@hotmail.com>
2024-02-10 19:55:01 -0600

    Screens login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:prueba1/screens/dashboard_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,13 +10,17 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool isLoading = false;
+
   final txtUser = TextFormField(
     keyboardType: TextInputType.emailAddress,
+    decoration: const InputDecoration(border: OutlineInputBorder()),
   );
 
   final pdwUser = TextFormField(
     keyboardType: TextInputType.text,
     obscureText: true,
+    decoration: const InputDecoration(border: OutlineInputBorder()),
   );
 
   @override
@@ -28,16 +34,64 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Stack(
           alignment: Alignment.center,
           children: [
-            Image.asset('images/loginlogo.png'),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 10),
-              color: Colors.black,
-              height: 200,
-              child: ListView(
-                shrinkWrap: true,
-                children: [txtUser, pdwUser],
+            Positioned(
+              top: 470,
+              child: Opacity(
+                opacity: .5,
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                  height: 200,
+                  width: MediaQuery.of(context).size.width * 9,
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      txtUser,
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      pdwUser
+                    ],
+                  ),
+                ),
               ),
-            )
+            ),
+            Image.asset('images/loginlogo.png'),
+            Positioned(
+                bottom: 50,
+                child: Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width * .9,
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      SignInButton(Buttons.Email, onPressed: () {
+                        setState(() {
+                          isLoading = !isLoading;
+                        });
+                        Future.delayed(new Duration(milliseconds: 5000), () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => new DashboardScreen(),
+                              ));
+                        });
+                      }),
+                      SignInButton(Buttons.Google, onPressed: () {}),
+                      SignInButton(Buttons.Facebook, onPressed: () {}),
+                      SignInButton(Buttons.GitHub, onPressed: () {}),
+                    ],
+                  ),
+                )),
+            isLoading
+                ? const Positioned(
+                    top: 260,
+                    child: CircularProgressIndicator(
+                      color: Colors.white,
+                    ))
+                : Container()
           ],
         ),
       ),
