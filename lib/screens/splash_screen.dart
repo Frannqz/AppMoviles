9e0611ebccae9db8ee073c6786b5494a8af2a4fa
Francisco Gonzalez <paco_glez02@hotmail.com>
2024-02-23 13:01:17 -0600

    Ajuste en splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -10,14 +10,15 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SplashView(
       backgroundColor: Colors.green[500],
-      logo: Image.network(
-          'https://lh3.googleusercontent.com/proxy/yiK1VSbW8NNaUkQ85MmBJaTcfiSBGTCgeNYX2t3NbRQl3vvISWAmB_VgK6m4tx2yJbHGpX1i_jT9hD1K8BADe1X0rC1QKEBfJLZAh9D8n5L_MdgxNv9RgBic8PTGtXbo3DDK',
-          height: 250),
-      loadingIndicator: Image.asset(
-        'images/load.gif',
-        height: 100, // Reducir el tamaño del GIF
-        width: 100,
+      logo: Image.asset(
+        'images/itcelaya.png',
+        height: 250,
       ),
+      // loadingIndicator: Image.asset(
+      //   'images/load.gif',
+      //   height: 100, // Reducir el tamaño del GIF
+      //   width: 100,
+      // ),
       done: Done(const LoginScreen(),
           animationDuration: const Duration(milliseconds: 1500)),
     );
