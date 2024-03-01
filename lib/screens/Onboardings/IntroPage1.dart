bd707e5f9b3d801a5afef8baa4889b2996e96e99
Francisco González Rodríguez <paco_glez02@hotmail.com>
2024-03-01 15:14:19 -0600

    Pantalla 1 del onboarding lista

--- lib/screens/Onboardings/IntroPage1.dart
+++ lib/screens/Onboardings/IntroPage1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class IntroPage1 extends StatelessWidget {
   const IntroPage1({super.key});
@@ -6,10 +8,74 @@ class IntroPage1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green[200],
-      child: const Center(
-        child: Text("Page 1"),
-      ),
-    );
+        color: Colors.green[200],
+        child: Column(children: [
+          Container(
+              width: 220,
+              margin: const EdgeInsets.only(top: 60, bottom: 30),
+              child: Image.asset("images/itcelaya.png")),
+          Container(
+            width: 250,
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            child: const Text(
+              "Tecnológico Nacional de México en Celaya",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black87,
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+            child: const Text(
+              "El Instituto Tecnológico de Celaya es una institución de educación superior localizado en la ciudad de Celaya, Gto. Es dependiente, al igual que varios institutos tecnológicos, orgánica y funcionalmente del Gobierno Federal.",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.black87,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 30, top: 15),
+                width: 100,
+                child: Lottie.network(
+                  "https://lottie.host/2940a4d8-a056-4994-96a9-ecd1bce02fac/x3o0tva0Zd.json",
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(right: 90.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      _launchURL(); // Abre el enlace cuando se toca el texto
+                    },
+                    child: Text(
+                      "Antonio García Cubas 600, Fovissste, 38010 Celaya, Gto.",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey[800],
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ]));
+  }
+
+  // Función para abrir enlace
+  _launchURL() async {
+    const url = 'https://maps.app.goo.gl/dZrYRq9YzTGzqzgN9';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'No se pudo abrir el enlace $url';
+    }
   }
 }
--- lib/screens/Onboardings/IntroPage3.dart
+++ lib/screens/Onboardings/IntroPage3.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class IntroPage3 extends StatelessWidget {
   const IntroPage3({super.key});
@@ -7,8 +8,15 @@ class IntroPage3 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.amber[200],
-      child: const Center(
-        child: Text("Page 3"),
+      child: Align(
+        alignment: Alignment.topCenter, // Alinear hacia arriba en el centro
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 20.0), // Agregar un espacio superior
+          child: Lottie.network(
+            "https://lottie.host/972700a1-ed80-4f5d-8946-808f4de12f45/kZ4dgYMthV.json",
+          ),
+        ),
       ),
     );
   }
